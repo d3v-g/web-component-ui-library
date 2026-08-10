3c90697ac2a67b3af8a23ae332436c31fa45e499
d3v-g <wang.jiaye.ti@gmail.com>
2026-08-10 16:40:59 +0100

    single build JS

--- dist/cdn/icons/index.js
+++
@@ -1,6 +0,0 @@
-import { Jet2IconInfo as e } from "./info.js";
-import { Jet2IconCheck as t } from "./check.js";
-import { Jet2IconWarningCircle as n } from "./warning-circle.js";
-import { Jet2IconX as r } from "./x.js";
-import { Jet2IconMagnifyingGlass as i } from "./magnifying-glass.js";
-export { t as Jet2IconCheck, e as Jet2IconInfo, i as Jet2IconMagnifyingGlass, n as Jet2IconWarningCircle, r as Jet2IconX };
